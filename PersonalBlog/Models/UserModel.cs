d42a2eb7cd2cbc5c55b0bc45edc377dcc158c53a
Renan Cunha <renancdev@gmail.com>
2023-11-16 11:20:52 -0300

    Update UserModel.cs
    teste de modelo de usuario onde o usuario passa a possuir uma lista de posts

--- PersonalBlog/Models/UserModel.cs
+++ PersonalBlog/Models/UserModel.cs
@@ -9,5 +9,6 @@ namespace PersonalBlog.Models
         public string? Email { get; set; }
         public string? Password { get; set; }
         public UserStatus Status { get; set; }
+        public List<PostModel> posts {get;}
     }
 }
